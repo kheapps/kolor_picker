fcee7b2c021dfbda8261ea219540022525324df5
bahaa <mansour.b@kheapps.com>
2021-06-26 19:28:37 +0200

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+# kolor_picker
+
+A simple color picker package for your Flutter apps.
+
+## Getting Started
+
---
+++ lib/kolor_picker.dart
@@ -0,0 +1,12 @@
+library kolor_picker;
+
+import 'package:flutter/material.dart';
+
+class KolorPicker extends StatelessWidget {
+  const KolorPicker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
