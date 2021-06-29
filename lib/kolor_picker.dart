c87c525c988d9b81e44cbc4225b3684d4a3aaf29
B4H44 <mansour.b@kheapps.com>
2021-06-29 20:14:38 +0200

    feat custom slider for color picking #1

--- lib/kolor_picker.dart
+++ lib/kolor_picker.dart
@@ -2,11 +2,53 @@ library kolor_picker;
 
 import 'package:flutter/material.dart';
 
+import 'src/picker_modal.dart';
+
 class KolorPicker extends StatelessWidget {
-  const KolorPicker({Key? key}) : super(key: key);
+  final String title;
+  final ButtonStyle? style;
+  final Function(Color)? onComplete;
+
+  static const ButtonStyle _defaultStyle = ButtonStyle();
+
+  const KolorPicker({
+    Key? key,
+    this.title = '',
+    this.style = _defaultStyle,
+    this.onComplete,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: ElevatedButton(
+        child: (title != '') ? Text(title) : Text("Open"),
+        style: style,
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            backgroundColor: Colors.transparent,
+            enableDrag: true,
+            builder: (ctx) => PickerModal(
+              onColorPicked: _onColorPicked,
+            ),
+          )
+            ..onError((e, stackTrace) => _onError(e, stackTrace))
+            ..whenComplete(() => _onComplete());
+        },
+      ),
+    );
+  }
+
+  _onColorPicked(Color c) {
+    print("chosen color : " + c.toString());
+  }
+
+  _onComplete() {
+    print("modal closed");
+  }
+
+  _onError(e, stackTrace) {
+    print("error" + e + " | " + stackTrace);
   }
 }
---
+++ lib/src/picker_modal.dart
@@ -0,0 +1,82 @@
+library picker_modal;
+
+import 'package:flutter/material.dart';
+
+class PickerModal extends StatefulWidget {
+  final Color baseColor;
+  final Function(Color)? onColorPicked;
+
+  const PickerModal({
+    Key? key,
+    this.baseColor = Colors.greenAccent,
+    this.onColorPicked,
+  }) : super(key: key);
+
+  @override
+  _PickerModalState createState() => _PickerModalState(this.baseColor);
+}
+
+class _PickerModalState extends State<PickerModal> {
+  Color _currentColor;
+
+  _PickerModalState(this._currentColor);
+
+  Color get currentColor => _currentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    // double w = MediaQuery.of(context).size.width;
+    final media = MediaQuery.of(context);
+    double h = media.size.height;
+    double bottomMargin = media.viewPadding.bottom;
+    return Container(
+      height: h * 0.67,
+      margin: EdgeInsets.only(
+        left: 10,
+        right: 10,
+        top: 0,
+        bottom: bottomMargin,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15.0)),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 50,
+              height: 5,
+              decoration: BoxDecoration(
+                color: Colors.black45,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 50,
+              height: 50,
+              margin: EdgeInsets.only(top: 10),
+              decoration: BoxDecoration(
+                color: _currentColor,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25),
+                ),
+              ),
+            ),
+            Slider(
+              value: 0,
+              onChanged: (v) {},
+              
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
