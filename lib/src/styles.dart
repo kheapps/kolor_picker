8365b19b25fd0e510bddae873ef1cfb761b5800f
B4H44 <mansour.b@kheapps.com>
2021-07-02 16:55:31 +0200

    added custom font and text style

---
+++ lib/src/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const TextStyle labelTextStyle = TextStyle(
+  fontFamily: 'Rubik',
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 17,
+);
+
+const TextStyle valueTextStyle = TextStyle(
+  fontFamily: 'Rubik',
+  color: Colors.blueGrey,
+  fontWeight: FontWeight.w700,
+  letterSpacing: 2,
+  fontSize: 15,
+);
